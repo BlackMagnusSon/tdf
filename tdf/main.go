292ac144f14554ff3e48027c63875ff0b2ee3e4b
dshimko <dshimko@tradingview.com>
2021-08-24 22:07:39 +0300

    s

--- tdf/main.go
+++ tdf/main.go
@@ -1,3 +1,12 @@
 package main
 
-import "tdf/timeconverter"
+import timeconverter "tdf/timeconverter"
+import "fmt"
+
+func main() {
+	var tested int64 = 0 
+	var time_To_Test =  timeconverter.ut{timestamps:"19999999",unixOrUdf:false}
+	var tested = time_To_Test.converter()
+	
+	fmt.Printf("tTime is: %s\n", k.Format("15:04:05.999")
+}
--- tdf/timeconverter/timeconverter.go
+++ tdf/timeconverter/timeconverter.go
@@ -1,1 +1,31 @@
 package timeconverter
+
+import (
+	"strconv"
+	"time"
+)
+
+type ut struct {
+	timestamps string
+	unixOrUdf  bool // "unix" if true, "udf" is false
+}
+
+func (p ut) converter() int64 {
+	if p.unixOrUdf == true {
+		timetemplate := "20060102T150405"
+		tm, err := time.Parse(p.timestamps, timetemplate[:len(p.timestamps)])
+		if err != nil {
+			panic(err)
+		} else {
+			return tm
+		}
+	} else {
+		i, err := strconv.ParseInt("1405544146", 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		tm := time.Unix(i, 0)
+		return tm
+	}
+	return 0
+}
