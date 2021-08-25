75bbdf373d1b72b38a16148e3c8347f5c5669c83
blackmagnusson <snimko2990@gmail.com>
2021-08-25 11:54:05 +0300

    1

--- tdf/main.go
+++ tdf/main.go
@@ -9,5 +9,6 @@ func main() {
 	var timeToTest = tc.Ut{Timestamps: "19999999", UnixOrUdf: false}
 	var k = timeToTest.Converter()
 
-	fmt.Printf("tTime is: %s\n", k.Format("15:04:05.999"))
+	fmt.Printf("Time is: %s\n", k.Format("15:04:05.999"))
+	fmt.Printf("Time is: %s\n", k.Format("15:04:05.999"))
 }
