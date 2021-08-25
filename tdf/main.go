9ebf1d8fb0154026b069f05ec42ef01f792fdc69
blackmagnusson <snimko2990@gmail.com>
2021-08-25 18:20:18 +0300

    test inputs

--- tdf/main.go
+++ tdf/main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	var source string
+	fmt.Scan(&source)
+	fmt.Printf("\n\n\n %s \n", source)
+
 	useUdf := flag.Bool("udf", false, "convert udf from udf or not")
 	useTime := flag.String("t", "20200115T", "time")
 	flag.Parse()
